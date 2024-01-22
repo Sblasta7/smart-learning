7a10c9911b34ef061ba000f952906a04a9bfeaec
Sblasta7 <ngozonathi@gmail.com>
2024-01-22 22:01:12 +0200

    init

---
+++ javascript/navbar.js
@@ -0,0 +1,8 @@
+const showSideBar = () =>{
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex'
+}
+const hideSideBar = () =>{
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none'
+}
